f945ac8a74e021befdaf822a7cc2ace3efde85a5
DesignDigital <kelwigamer@gmail.com>
2025-05-01 22:46:16 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.getElementById('menu-icon');
+const menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', () => {
+menu.classList.toggle('active');
+});
+
